639a51344c10c75436f18260725ac47271b26557
Philipp Chapkovski <chapkovski@gmail.com>
2024-10-24 17:22:11 +0200

    switching to dark mode

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,11 +7,36 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
 // Composables
 import { createVuetify } from 'vuetify'
+// Import Vuetify themes if needed
+export default  createVuetify({
+  theme: {
+    defaultTheme: 'dark',
+    themes: {
+      dark: {
+        dark: true, // This sets the theme to dark mode
+        colors: {
+          primary: '#1e88e5',
+          secondary: '#ff4081',
+          accent: '#82B1FF',
+        },
+      },
+    },
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-export default createVuetify({
-  //
-})
+// export default createVuetify({
+//   //
+// })
